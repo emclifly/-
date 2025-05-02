bfcab0efc3bfa3ebe5982b3ffdbd2638efdb86c3
emclifly <topemil32@gmail.com>
2025-05-02 22:43:09 +0700

    Update censor.js

--- server/utils/censor.js
+++ server/utils/censor.js
@@ -1,19 +1,11 @@
-const badWords = [
-  'badword1',
-  'badword2',
-  // …добавьте свои
-];
+import Filter from 'bad-words';
 
-export function hasBadWords(text = '') {
-  const lower = text.toLowerCase();
-  return badWords.some(w => lower.includes(w));
+const filter = new Filter();
+
+export function cleanText(text) {
+  return filter.clean(text);
 }
 
-export function cleanText(text = '') {
-  let result = text;
-  badWords.forEach(w => {
-    const regex = new RegExp(w, 'gi');
-    result = result.replace(regex, '*'.repeat(w.length));
-  });
-  return result;
+export function hasBadWords(text) {
+  return filter.isProfane(text);
 }
